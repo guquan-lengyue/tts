61537266e2f1b89543ae868c34d6432334b3a592
孤泉冷月 <guquanlengyue@gmail.com>
2023-09-28 12:05:12 +0800

    ssml错误修复

--- tts_test.go
+++ tts_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bytes"
 	"github.com/go-playground/assert/v2"
 	"net/http"
 	"net/http/httptest"
@@ -9,10 +10,13 @@ import (
 	"testing"
 )
 
+const params = `tex=%25E6%2589%258B%25EF%25BC%258C%25E7%25BA%25B7%25E7%25BA%25B7%25E8%25A2%25AB%25E6%2596%25A9%25E6%259D%2580%25E7%259A%2584%25E4%25B8%2580%25E5%25B9%25B2%25E4%25BA%258C%25E5%2587%2580%25E3%2580%2582&spd=9.5&vn=zh-CN-YunjianNeural`
+
 func Test_TTS(t *testing.T) {
 	r := setRouter()
 	w := httptest.NewRecorder()
-	req, _ := http.NewRequest("GET", "/?tex=你好世界&spd=20", nil)
+	reader := bytes.NewReader([]byte(params))
+	req, _ := http.NewRequest("POST", "/", reader)
 	r.ServeHTTP(w, req)
 	assert.Equal(t, 200, w.Code)
 	open, _ := os.OpenFile("text.webm", os.O_RDWR|os.O_CREATE|os.O_APPEND, os.ModePerm)
@@ -23,7 +27,7 @@ func Test_TTS(t *testing.T) {
 }
 
 func Test_UrlParams(t *testing.T) {
-	params := `tex=%25E6%2589%258B%25EF%25BC%258C%25E7%25BA%25B7%25E7%25BA%25B7%25E8%25A2%25AB%25E6%2596%25A9%25E6%259D%2580%25E7%259A%2584%25E4%25B8%2580%25E5%25B9%25B2%25E4%25BA%258C%25E5%2587%2580%25E3%2580%2582&spd=9.5&vn=zh-CN-YunjianNeural`
+
 	parse, err := url.ParseQuery(params)
 	if err != nil {
 		t.Error(err)
