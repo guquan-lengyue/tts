beae16a48125934d7cff5e010e03352f42710512
孤泉冷月 <guquanlengyue@gmail.com>
2023-09-28 11:20:53 +0800

    改为post

--- main.go
+++ main.go
@@ -4,14 +4,13 @@ import (
 	"github.com/gin-gonic/gin"
 	"ms_edge_tts/src"
 	"net/http"
-	"strconv"
 )
 
 var tts = src.NewMsEdgeTTS(true)
 
 func main() {
 	r := setRouter()
-	err := r.Run("127.0.0.1:2580")
+	err := r.Run("0.0.0.0:2580")
 	if err != nil {
 		panic(err)
 	}
@@ -20,19 +19,24 @@ func main() {
 func setRouter() *gin.Engine {
 	r := gin.Default()
 
-	r.GET("/", receive)
+	r.POST("", receive)
 	return r
 }
+
+type body struct {
+	Text      string `json:"tex"`
+	Speed     int    `json:"spd"`
+	VoiceName string `json:"vn"`
+}
+
 func receive(c *gin.Context) {
-	text := c.DefaultQuery("tex", "")                        // text
-	speakSpeed := c.DefaultQuery("spd", "0")                 // speak speed
-	voiceName := c.DefaultQuery("vn", "zh-CN-YunyangNeural") // voiceName
-	spd, err := strconv.Atoi(speakSpeed)
+	var form body
+	err := c.ShouldBind(&form)
 	if err != nil {
 		c.JSON(http.StatusBadRequest, map[string]error{"error": err})
 	}
-	tts.SetMetaData(voiceName, src.WEBM_24KHZ_16BIT_MONO_OPUS, 0, spd, 0)
-	speechCh := tts.TextToSpeech(text)
+	tts.SetMetaData(form.VoiceName, src.WEBM_24KHZ_16BIT_MONO_OPUS, 0, form.Speed, 0)
+	speechCh := tts.TextToSpeech(form.Text)
 	c.Header("Context-Type", "Content-Type: audio/webm")
 	for ch := range speechCh {
 		_, err := c.Writer.Write(ch)
